c63608d38ad87927b3695c7ee2c86f89df899778
SongSeoHui <midnight3655@gmail.com>
2022-02-28 00:35:11 +0900

    JS : Navbar menu click
    navbar menu button클릭하면 원하는 곳으로 스크롤링
    요소 이름이 같도록 지정

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ document.addEventListener("scroll",() => {
         navbar.classList.remove("navbar--dark");
     }
 })
+
+//navbar menu button클릭하면 원하는 곳으로 스크롤링
+const navbarMenu=document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click",(event)=>{
+    console.log(event.target.dataset.link);//클릭한 요소의 data-link: ex)#home
+    const target=event.target;//클릭한 요소
+    const link=target.dataset.link;//data-link: ex)#home
+    if(link===null){//우리가 지정안한 버튼일때
+        return;//그냥 반환시킴
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo=document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+    //.scrollIntoView(): 그 요소가 있는 화면으로 스크롤이동(요소이름이 같은 화면으로 이동:#home/ navbar__menu__item의 data-link가 #home===화면이름도 #home)
+})
