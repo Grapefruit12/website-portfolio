c61be2feefd7374da9e249a599e325d404f9d4c4
Grapefruit12 <midnight3655@gmail.com>
2022-03-07 19:17:11 +0900

    JS : 스크롤시 메뉴 활성화

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ navbarMenu.addEventListener("click",(event)=>{
     navbarMenu.classList.remove("open");
     scrollIntoView(link);
     
+    
 });
 
 //핸드폰화면에서 navbar toggle button
@@ -129,7 +130,67 @@ workBtnContainer.addEventListener("click",(e) => {
     },300);
 })
 
+
+
+//1. 모든 섹션 요소들을 가지고 온다
+//2. IntersectionObserver를 이용해서 모든 섹션을 관찰한다
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds=[
+    "#home",
+    "#about",
+    "#projects",
+    "#contact",
+];
+const sections=sectionIds.map(id => document.querySelector(id));
+//map() 메서드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환합니다.
+const navItems=sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+//console.log(sections);
+//console.log(navItems);
+let selectedNavIndex=0;
+let selectedNavItem=navItems[0];
+//console.log(selectedNavItem);
+function selectNavItem(selected){
+    
+    selectedNavItem.classList.remove("active");
+    //console.log(selectedNavItem);
+    selectedNavItem=selected;
+    selectedNavItem.classList.add("active");
+}
 function scrollIntoView(selector){//요소가 있는 화면으로 스크롤이동
     const scrollTo=document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:"smooth"});//.scrollIntoView(): 그 요소가 있는 화면으로 스크롤이동(요소이름이 같은 화면으로 이동:#home/ navbar__menu__item의 data-link가 #home===화면이름도 #home)
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+const observerOptions={
+    root:null,
+    rootMargin: "0px",
+    threshold: 0.3,
+};
+const observerCallback=(entries,observer)=>{
+    entries.forEach(entry=>{
+        if(!entry.isintersecting && entry.intersectionRatio >0){//진입하지않을때,빠져나갈때(!===not)
+            const index=sectionIds.indexOf(`#${entry.target.id}`);
+            //console.log(index,entry.target.id);
+            
+            //스크롤링이 아래로 되어서 페이지가 올라옴
+            if(entry.boundingClientRect.y<0){
+                selectedNavIndex=index+1;
+            }else{
+                selectedNavIndex=index-1;
+            }
+            
+        }
+    });
+};
+const observer=new IntersectionObserver(observerCallback,observerOptions);
+sections.forEach(section=>observer.observe(section));
+
+window.addEventListener("wheel", () => { //wheel: "사용자"가 스스로 스크롤링 할때 발생되는 이벤트(scroll은 "모든 스크롤"이 해당하는 이벤트가 발생할때마다 생성되는 이벤트)
+    if(window.scrollY===0){
+        selectedNavIndex=0;
+    }else if(window.scrollY + window.innerHeight===document.body.clientHeight){
+        selectedNavIndex=navItems.length-1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
